8c199202bb2b9281ec4720fd97f86c53602afb37
narayanbehera14 <beheranarayancs222330@gmail.com>
2026-01-16 23:53:23 +0530

    updated

--- your_tour_guide/src/pages/SignIn.jsx
+++ your_tour_guide/src/pages/SignIn.jsx
@@ -1,18 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const SignIn = () => {
   return (
-    <div className="signin-container">
-      <h2>Sign In</h2>
-
-      <form className="signin-form">
-        <input type="email" placeholder="Email" required />
-        <input type="password" placeholder="Password" required />
-
-        <button type="submit">Sign In</button>
-      </form>
+    <div>
+      <h1>sign in</h1>
     </div>
-  );
-};
+  )
+}
 
-export default SignIn;
+export default SignIn
